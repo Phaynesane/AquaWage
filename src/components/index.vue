42abbc308c115d079eea5ba3f7ce5c735e3eaa7e
kiyushixxx <144781643+kiyushixxx@users.noreply.github.com>
2023-11-16 21:22:57 +0800

    this

--- src/components/index.vue
+++ src/components/index.vue
@@ -3,10 +3,10 @@
           <!-- partial:partials/_navbar.html -->
       <nav class="navbar default-layout col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
         <div class="text-center navbar-brand-wrapper d-flex align-items-top justify-content-center">
-          <a class="navbar-brand brand-logo" href="index.html">
-            <img src="assets/images/logo.svg" alt="logo" /> </a>
-          <a class="navbar-brand brand-logo-mini" href="index.html">
-            <img src="assets/images/logo-mini.svg" alt="logo" /> </a>
+          <a class="navbar-brand brand-logo" href="index.vue">
+            <img src="assets/images/logo/logos.png" alt="AquaWage" /> </a>
+          <a class="navbar-brand brand-logo-mini" href="index.vue">
+            <img src="assets/images/logo/logos.png" alt="Aquawage" /> </a>
         </div>
         <div class="navbar-menu-wrapper d-flex align-items-center">
           <ul class="navbar-nav">
@@ -74,10 +74,10 @@
             </li>
             <li class="nav-item dropdown d-none d-xl-inline-block user-dropdown">
               <a class="nav-link dropdown-toggle" id="UserDropdown" href="#" data-toggle="dropdown" aria-expanded="false">
-                <img class="img-xs rounded-circle" src="assets/images/faces/face8.jpg" alt="Profile image"> </a>
+                <img class="img-xs rounded-circle" src="assets/images/face/face1.jpg" alt="Profile image"> </a>
               <div class="dropdown-menu dropdown-menu-right navbar-dropdown" aria-labelledby="UserDropdown">
                 <div class="dropdown-header text-center">
-                  <img class="img-md rounded-circle" src="assets/images/faces/face8.jpg" alt="Profile image">
+                  <img class="img-md rounded-circle" src="assets/images/face/face1.jpg" alt="Profile image">
                   <p class="mb-1 mt-3 font-weight-semibold">Reanne Martinez</p>
                   <p class="font-weight-light text-muted mb-0">ReanneMartinez@gmail.com</p>
                 </div>
@@ -102,7 +102,7 @@
             <li class="nav-item nav-profile">
               <a href="#" class="nav-link">
                 <div class="profile-image">
-                  <img class="img-xs rounded-circle" src="assets/images/faces/face8.jpg" alt="profile image">
+                  <img class="img-xs rounded-circle" src="assets/images/face/face1.jpg" alt="profile image">
                   <div class="dot-indicator bg-success"></div>
                 </div>
                 <div class="text-wrapper">
--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -2,10 +2,10 @@
      <div class="container-scroller">
         <nav class="navbar default-layout col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
   <div class="text-center navbar-brand-wrapper d-flex align-items-top justify-content-center">
-    <a class="navbar-brand brand-logo" href="index.html">
-      <img src="assets/images/logo.svg" alt="logo" /> </a>
-    <a class="navbar-brand brand-logo-mini" href="index.html">
-      <img src="assets/images/logo-mini.svg" alt="logo" /> </a>
+    <a class="navbar-brand brand-logo" href="index.vue">
+      <img src="assets/images/logo/logos.png" alt="AquaWage" /> </a>
+    <a class="navbar-brand brand-logo-mini" href="index.vue">
+      <img src="assets/images/logo/logos.png" alt="AquaWage" /> </a>
   </div>
   <div class="navbar-menu-wrapper d-flex align-items-center">
     <form class="ml-auto search-form d-none d-md-block" action="#">
@@ -67,10 +67,10 @@
       </li>
       <li class="nav-item dropdown d-none d-xl-inline-block user-dropdown">
         <a class="nav-link dropdown-toggle" id="UserDropdown" href="#" data-toggle="dropdown" aria-expanded="false">
-          <img class="img-xs rounded-circle" src="assets/images/faces/face8.jpg" alt="Profile image"> </a>
+          <img class="img-xs rounded-circle" src="assets/images/face/face1.jpg" alt="Profile image"> </a>
         <div class="dropdown-menu dropdown-menu-right navbar-dropdown" aria-labelledby="UserDropdown">
           <div class="dropdown-header text-center">
-            <img class="img-md rounded-circle" src="assets/images/faces/face8.jpg" alt="Profile image">
+            <img class="img-md rounded-circle" src="assets/images/face/face1.jpg" alt="Profile image">
             <p class="mb-1 mt-3 font-weight-semibold">Reanne Martinez</p>
             <p class="font-weight-light text-muted mb-0">ReanneMartinez@gmail.com</p>
           </div>
